c014ed59c2f939d02c529b4f7ee11680d46bf9cb
Evan Griffith <evan@evangriffith.net>
2024-04-01 11:06:06 -0700

    Adjusted incorrect class syntax in JSX components

--- src/app/ui/BackgroundAnimation.jsx
+++ src/app/ui/BackgroundAnimation.jsx
@@ -1,9 +1,9 @@
 export default function BackgroundAnimation() {
   return (
     <div className=" absolute size-full overflow-hidden">
-      <div class="animate-blob absolute -left-4 top-0 size-6/12 rounded-full bg-violet-300 opacity-40 mix-blend-multiply blur-2xl filter"></div>
-      <div class="animate-blob animation-delay-6000 absolute inset-x-2/3 inset-y-2/3 top-96 size-8/12 rounded-full bg-teal-300 opacity-40 mix-blend-multiply blur-2xl filter"></div>
-      <div class="animate-blob animation-delay-10000 absolute -bottom-32 left-64 size-6/12 rounded-full bg-blue-300 opacity-40 mix-blend-multiply blur-2xl filter"></div>
+      <div className="animate-blob absolute -left-4 top-0 size-6/12 rounded-full bg-violet-300 opacity-40 mix-blend-multiply blur-2xl filter"></div>
+      <div className="animate-blob animation-delay-6000 absolute inset-x-2/3 inset-y-2/3 top-96 size-8/12 rounded-full bg-teal-300 opacity-40 mix-blend-multiply blur-2xl filter"></div>
+      <div className="animate-blob animation-delay-10000 absolute -bottom-32 left-64 size-6/12 rounded-full bg-blue-300 opacity-40 mix-blend-multiply blur-2xl filter"></div>
     </div>
   );
 }
