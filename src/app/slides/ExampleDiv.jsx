a49c00934dbe0b496961ba44d4a461f6a4afe4a0
Evan Griffith <evan@evangriffith.net>
2024-03-31 21:52:26 -0700

    Some progress on div example styles

--- src/app/slides/ExampleDiv.jsx
+++ src/app/slides/ExampleDiv.jsx
@@ -6,25 +6,29 @@ export default function ExampleDiv() {
       <h1 className="text-3xl font-bold lg:text-7xl 2xl:text-9xl">
         Let&apos;s check out an example div:
       </h1>
-      <div className="flex flex-row justify-around">
-        <div className="m-2 2xl:m-5">
-          <h3 className="mx-5 mb-5 mt-7 2xl:mt-20 font-mono text-5xl font-bold">Default</h3>
+      <div className="flex flex-row justify-around h-4/5">
+        <div className="m-2 size-full py-10 pt-0 2xl:m-5 2xl:px-16">
+          <h3 className="mx-5 mb-5 mt-7 font-mono text-5xl font-bold 2xl:mt-20">
+            Default
+          </h3>
           <div className="mockup-browser bg-base-300 border shadow-2xl">
             <div className="mockup-browser-toolbar">
               <div className="input">{`//localhost:3000`}</div>
             </div>
-            <div className="flex justify-center bg-neutral-50 px-4 py-16">
+            <div className="flex justify-center bg-neutral-50 py-16 2xl:py-32">
               <TailwindDivExample />
             </div>
           </div>
         </div>
-        <div className="m-5">
-          <h3 className="mx-5 mb-5 mt-7 2xl:mt-20 font-mono text-5xl font-bold">On Hover</h3>
+        <div className="m-2 size-full py-10 pt-0 2xl:m-5 2xl:px-16">
+          <h3 className="mx-5 mb-5 mt-7 font-mono text-5xl font-bold 2xl:mt-20">
+            On Hover
+          </h3>
           <div className="mockup-browser bg-base-300 border shadow-2xl">
             <div className="mockup-browser-toolbar">
               <div className="input">{`//localhost:3000`}</div>
             </div>
-            <div className="flex justify-center bg-neutral-50 px-4 py-16">
+            <div className="flex justify-center bg-neutral-50 py-16 2xl:py-32">
               <TailwindDivExample hoverState={true} />
             </div>
           </div>
