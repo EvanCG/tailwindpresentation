2f243e5a00f331c9fd9912ffca7a15bb046e8914
Evan Griffith <evan@evangriffith.net>
2024-04-01 11:14:13 -0700

    Underline link on hover & adjusted order to bullets

--- src/app/slides/HowToUseTailwind.jsx
+++ src/app/slides/HowToUseTailwind.jsx
@@ -7,11 +7,17 @@ export default function HowToUseTailwind() {
         How do I use Tailwind?
       </h1>
       <h3 className="mt-10 py-5 text-5xl font-bold italic">
-        Visit <a href="https://tailwindcss.com/" className="text-blue-600">tailwindcss.com</a> to view the
-        docs
+        Visit{" "}
+        <a
+          href="https://tailwindcss.com/"
+          className="text-blue-600 hover:underline"
+        >
+          tailwindcss.com
+        </a>{" "}
+        to view the docs
       </h3>
       <div className="flex items-center justify-between lg:flex-row">
-      <div className="flex w-2/5 flex-col items-center justify-center p-2 left-5">
+        <div className="left-5 flex w-2/5 flex-col items-center justify-center p-2">
           <div className="mockup-browser bg-base-300 -rotate-3 border shadow-2xl ">
             <div className="mockup-browser-toolbar">
               <div className="input">{`tailwindcss.com/`}</div>
@@ -26,13 +32,36 @@ export default function HowToUseTailwind() {
             </div>
           </div>
         </div>
-        <ol className="mt-5 w-3/5 list-decimal self-start ml-36 text-lg lg:text-3xl 2xl:mt-10 2xl:self-center 2xl:text-7xl font-bold">
-          <li className="my-7 2xl:my-20">Tailwind CLI<span className="font-normal text-gray-600"> - best general purpose</span></li>
-          <li className="my-7 2xl:my-20">PostCSS<span className="font-normal text-gray-600"> - ideal for build tools</span></li>
-          <li className="my-7 2xl:my-20">Play CDN<span className="font-normal text-gray-600"> - don&apos;t use in production!</span></li>
-          <li className="my-7 2xl:my-20">Framework-specific Guides<span className="font-normal text-gray-600"> - many popular frameworks included</span></li>
+        <ol className="ml-36 mt-5 w-3/5 list-decimal self-start text-lg font-bold lg:text-3xl 2xl:mt-10 2xl:self-center 2xl:text-7xl">
+          <li className="my-7 2xl:my-20">
+            Tailwind CLI
+            <span className="font-normal text-gray-600">
+              {" "}
+              - best general purpose
+            </span>
+          </li>
+          <li className="my-7 2xl:my-20">
+            PostCSS
+            <span className="font-normal text-gray-600">
+              {" "}
+              - ideal for build tools
+            </span>
+          </li>
+          <li className="my-7 2xl:my-20">
+            Framework-specific Guides
+            <span className="font-normal text-gray-600">
+              {" "}
+              - many popular frameworks included
+            </span>
+          </li>
+          <li className="my-7 2xl:my-20">
+            Play CDN
+            <span className="font-normal text-gray-600">
+              {" "}
+              - don&apos;t use in production!
+            </span>
+          </li>
         </ol>
-        
       </div>
     </div>
   );
