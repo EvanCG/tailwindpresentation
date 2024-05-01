604952d14bae4f36586cfe0df3752820ee2df97d
Evan Griffith <evan@evangriffith.net>
2024-05-01 11:42:12 -0700

    Dynamic date of the presentation

--- src/app/slides/TitleSlide.jsx
+++ src/app/slides/TitleSlide.jsx
@@ -1,13 +1,32 @@
 import TailwindCSSLogo from "@/app/ui/logos/TailwindCSSLogo";
 
 export default function TitleSlide() {
+  const currentDate = new Date();
+
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const writtenDate = `${monthNames[currentDate.getMonth()]} ${currentDate.getFullYear()}`;
+
   return (
-    <div className="h-full w-full rounded-2xl opacity-85 bg-neutral-100 p-16 pt-20 shadow-lg flex flex-col justify-center">
+    <div className="flex h-full w-full flex-col justify-center rounded-2xl bg-neutral-100 p-16 pt-20 opacity-85 shadow-lg">
       <div className="w-3/5">
         <TailwindCSSLogo />
       </div>
       <h3 className="mt-20 text-5xl font-bold">Evan Griffith</h3>
-      <h4 className="mt-5 text-5xl">April 2024</h4>
+      <h4 className="mt-5 text-5xl">{writtenDate}</h4>
     </div>
   );
 }
